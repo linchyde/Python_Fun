afc778a1fb7d2ad939d2377e51b05d41c28bc3ec
lincoln Hyde <73680773+linchyde@users.noreply.github.com>
2022-07-19 14:55:03 +1000

    Python
    Learning

---
+++ main.py
@@ -0,0 +1,17 @@
+
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+for row in picture:
+  for pixel in row:
+    if pixel == 0:
+      print(' ', end='')
+    elif pixel == 1:
+      print('*', end='')
+  print('')
+  
